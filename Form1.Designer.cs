fb9fc325b5e9f319fc5cf0b3de453cfc04958c28
jerkerwallstrom <125583963+jerkerwallstrom@users.noreply.github.com>
2025-11-14 15:43:55 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace RangordnaCVs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKeyWord = new System.Windows.Forms.Label();
+            this.btnKeyWord = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblKeywordFile = new System.Windows.Forms.Label();
+            this.lblAssignmentDesc = new System.Windows.Forms.Label();
+            this.btnAssignmentDesc = new System.Windows.Forms.Button();
+            this.lblAssigmentFile = new System.Windows.Forms.Label();
+            this.lblSelectedCVfolder = new System.Windows.Forms.Label();
+            this.btnCVFolder = new System.Windows.Forms.Button();
+            this.lblCandidateFolder = new System.Windows.Forms.Label();
+            this.btnStartEvaluation = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.lblPythonFile = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnClearOutput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblKeyWord
+            // 
+            this.lblKeyWord.AutoSize = true;
+            this.lblKeyWord.Location = new System.Drawing.Point(18, 26);
+            this.lblKeyWord.Name = "lblKeyWord";
+            this.lblKeyWord.Size = new System.Drawing.Size(88, 13);
+            this.lblKeyWord.TabIndex = 0;
+            this.lblKeyWord.Text = "Välj nyckelordsfil:";
+            // 
+            // btnKeyWord
+            // 
+            this.btnKeyWord.Location = new System.Drawing.Point(112, 21);
+            this.btnKeyWord.Name = "btnKeyWord";
+            this.btnKeyWord.Size = new System.Drawing.Size(75, 23);
+            this.btnKeyWord.TabIndex = 1;
+            this.btnKeyWord.Text = "Nyckelord";
+            this.btnKeyWord.UseVisualStyleBackColor = true;
+            this.btnKeyWord.Click += new System.EventHandler(this.btnKeyWord_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblKeywordFile
+            // 
+            this.lblKeywordFile.AutoSize = true;
+            this.lblKeywordFile.Location = new System.Drawing.Point(18, 59);
+            this.lblKeywordFile.Name = "lblKeywordFile";
+            this.lblKeywordFile.Size = new System.Drawing.Size(198, 13);
+            this.lblKeywordFile.TabIndex = 2;
+            this.lblKeywordFile.Text = "C:\\Dev\\TestData\\weightedkeywords.txt";
+            // 
+            // lblAssignmentDesc
+            // 
+            this.lblAssignmentDesc.AutoSize = true;
+            this.lblAssignmentDesc.Location = new System.Drawing.Point(18, 129);
+            this.lblAssignmentDesc.Name = "lblAssignmentDesc";
+            this.lblAssignmentDesc.Size = new System.Drawing.Size(147, 13);
+            this.lblAssignmentDesc.TabIndex = 5;
+            this.lblAssignmentDesc.Text = "C:\\Dev\\TestData\\jobdesc.txt";
+            // 
+            // btnAssignmentDesc
+            // 
+            this.btnAssignmentDesc.Location = new System.Drawing.Point(152, 91);
+            this.btnAssignmentDesc.Name = "btnAssignmentDesc";
+            this.btnAssignmentDesc.Size = new System.Drawing.Size(122, 23);
+            this.btnAssignmentDesc.TabIndex = 4;
+            this.btnAssignmentDesc.Text = "Uppdragsbeskrivning";
+            this.btnAssignmentDesc.UseVisualStyleBackColor = true;
+            this.btnAssignmentDesc.Click += new System.EventHandler(this.btnAssignmentDesc_Click);
+            // 
+            // lblAssigmentFile
+            // 
+            this.lblAssigmentFile.AutoSize = true;
+            this.lblAssigmentFile.Location = new System.Drawing.Point(18, 96);
+            this.lblAssigmentFile.Name = "lblAssigmentFile";
+            this.lblAssigmentFile.Size = new System.Drawing.Size(128, 13);
+            this.lblAssigmentFile.TabIndex = 3;
+            this.lblAssigmentFile.Text = "Välj uppdragsbeskrivning:";
+            // 
+            // lblSelectedCVfolder
+            // 
+            this.lblSelectedCVfolder.AutoSize = true;
+            this.lblSelectedCVfolder.Location = new System.Drawing.Point(18, 197);
+            this.lblSelectedCVfolder.Name = "lblSelectedCVfolder";
+            this.lblSelectedCVfolder.Size = new System.Drawing.Size(110, 13);
+            this.lblSelectedCVfolder.TabIndex = 8;
+            this.lblSelectedCVfolder.Text = "C:\\Dev\\TestData\\CV";
+            // 
+            // btnCVFolder
+            // 
+            this.btnCVFolder.Location = new System.Drawing.Point(152, 159);
+            this.btnCVFolder.Name = "btnCVFolder";
+            this.btnCVFolder.Size = new System.Drawing.Size(122, 23);
+            this.btnCVFolder.TabIndex = 7;
+            this.btnCVFolder.Text = "CV katalog";
+            this.btnCVFolder.UseVisualStyleBackColor = true;
+            this.btnCVFolder.Click += new System.EventHandler(this.btnCVFolder_Click);
+            // 
+            // lblCandidateFolder
+            // 
+            this.lblCandidateFolder.AutoSize = true;
+            this.lblCandidateFolder.Location = new System.Drawing.Point(18, 164);
+            this.lblCandidateFolder.Name = "lblCandidateFolder";
+            this.lblCandidateFolder.Size = new System.Drawing.Size(82, 13);
+            this.lblCandidateFolder.TabIndex = 6;
+            this.lblCandidateFolder.Text = "Välj CV katalog:";
+            // 
+            // btnStartEvaluation
+            // 
+            this.btnStartEvaluation.Location = new System.Drawing.Point(21, 240);
+            this.btnStartEvaluation.Name = "btnStartEvaluation";
+            this.btnStartEvaluation.Size = new System.Drawing.Size(166, 23);
+            this.btnStartEvaluation.TabIndex = 9;
+            this.btnStartEvaluation.Text = "Starta utvärdering";
+            this.btnStartEvaluation.UseVisualStyleBackColor = true;
+            this.btnStartEvaluation.Click += new System.EventHandler(this.btnStartEvaluation_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(335, 12);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxOutput.Size = new System.Drawing.Size(430, 416);
+            this.textBoxOutput.TabIndex = 10;
+            this.textBoxOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblPythonFile
+            // 
+            this.lblPythonFile.AutoSize = true;
+            this.lblPythonFile.Location = new System.Drawing.Point(21, 409);
+            this.lblPythonFile.Name = "lblPythonFile";
+            this.lblPythonFile.Size = new System.Drawing.Size(53, 13);
+            this.lblPythonFile.TabIndex = 11;
+            this.lblPythonFile.Text = "Pythonfile";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(24, 283);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(163, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 12;
+            this.progressBar1.Visible = false;
+            // 
+            // btnClearOutput
+            // 
+            this.btnClearOutput.Location = new System.Drawing.Point(21, 325);
+            this.btnClearOutput.Name = "btnClearOutput";
+            this.btnClearOutput.Size = new System.Drawing.Size(85, 23);
+            this.btnClearOutput.TabIndex = 13;
+            this.btnClearOutput.Text = "Rensa resultat";
+            this.btnClearOutput.UseVisualStyleBackColor = true;
+            this.btnClearOutput.Click += new System.EventHandler(this.btnClearOutput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClearOutput);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblPythonFile);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.btnStartEvaluation);
+            this.Controls.Add(this.lblSelectedCVfolder);
+            this.Controls.Add(this.btnCVFolder);
+            this.Controls.Add(this.lblCandidateFolder);
+            this.Controls.Add(this.lblAssignmentDesc);
+            this.Controls.Add(this.btnAssignmentDesc);
+            this.Controls.Add(this.lblAssigmentFile);
+            this.Controls.Add(this.lblKeywordFile);
+            this.Controls.Add(this.btnKeyWord);
+            this.Controls.Add(this.lblKeyWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKeyWord;
+        private System.Windows.Forms.Button btnKeyWord;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblKeywordFile;
+        private System.Windows.Forms.Label lblAssignmentDesc;
+        private System.Windows.Forms.Button btnAssignmentDesc;
+        private System.Windows.Forms.Label lblAssigmentFile;
+        private System.Windows.Forms.Label lblSelectedCVfolder;
+        private System.Windows.Forms.Button btnCVFolder;
+        private System.Windows.Forms.Label lblCandidateFolder;
+        private System.Windows.Forms.Button btnStartEvaluation;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label lblPythonFile;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnClearOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace RangordnaCVs
+{
+    public partial class Form1 : Form
+    {
+        private string keywordFile = "C:\\Dev\\TestData\\weightedkeywords.txt";
+        private string jobdescFile = "C:\\Dev\\TestData\\jobdesc.txt";
+        private string cvFolder = "C:\\Dev\\TestData\\CV";
+        private string PythonexePath = @"C:\Dev\Python\RangordnaCV\dist\RangordnaCV.exe";
+        private string deflogFile = "RangordnaCV_log.docx";
+        private string logFile = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            string exePath = AppContext.BaseDirectory;
+            lblPythonFile.Text = exePath;
+            string pyFileName = Path.GetFileName(PythonexePath);
+            string tmpPython = exePath + pyFileName;
+            if (File.Exists(tmpPython))
+            {
+                PythonexePath = tmpPython;
+            }
+            else
+            {
+                PythonexePath = SelectFile(PythonexePath, "Välj exe-fil för rangordning");
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            deflogFile = Path.Combine(desktopPath, deflogFile);
+            logFile = FixLogFile(deflogFile);
+            lblPythonFile.Text = PythonexePath;
+            InitDefaultPath();
+        }
+
+        private void InitDefaultPath()
+        {
+            string exePath = AppContext.BaseDirectory;
+            keywordFile = Path.Combine(exePath, "NyckelOrd.txt");
+            jobdescFile = Path.Combine(exePath, "Arbetsbeskrivning.txt");
+            cvFolder = exePath + "CV";
+            lblSelectedCVfolder.Text = cvFolder;
+            lblKeywordFile.Text = keywordFile;
+            lblAssignmentDesc.Text = jobdescFile;
+        }
+
+        private string FixLogFile(string aLogFile)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            aLogFile = Path.Combine(desktopPath, aLogFile);
+            //if (File.Exists(aLogFile))
+            //{
+            //    File.Delete(aLogFile);
+            //}
+            int counter = 0;
+            string fileNameWithoutExt = Path.GetFileNameWithoutExtension(aLogFile);
+            string ext = Path.GetExtension(aLogFile);
+            while (File.Exists(aLogFile))
+            {
+                counter++;
+                string dir = Path.GetDirectoryName(aLogFile);
+                aLogFile = Path.Combine(dir, fileNameWithoutExt + "_" + counter.ToString() + ext);
+            }
+            return aLogFile;
+        }
+
+        private void btnKeyWord_Click(object sender, EventArgs e)
+        {
+            keywordFile = SelectFile(keywordFile, "Välj fil med nyckelord");
+            lblKeywordFile.Text = keywordFile;
+        }
+
+        private string SelectFile(string defFile, string header)
+        {
+            openFileDialog1.Title = header;
+            openFileDialog1.DefaultExt = ".txt";
+            openFileDialog1.FileName = defFile;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                return openFileDialog1.FileName;
+            }
+            return defFile;
+        }
+
+        private void btnAssignmentDesc_Click(object sender, EventArgs e)
+        {
+            jobdescFile = SelectFile(jobdescFile, "Välj fil för arbetsbeskrivning");
+            lblAssignmentDesc.Text = jobdescFile;
+        }
+
+        private void btnCVFolder_Click(object sender, EventArgs e)
+        {
+            string tmpF = ChooseFolder();
+            if (!string.IsNullOrEmpty(tmpF))
+            {
+                cvFolder = tmpF;
+                lblSelectedCVfolder.Text = cvFolder;
+            }
+        }
+
+        private string ChooseFolder()
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                return folderBrowserDialog1.SelectedPath;
+            }
+            return "";
+        }
+
+        private bool FileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        private bool ValidateFiles()
+        {
+            if (!FileExists(PythonexePath)) return false;
+            if (!FileExists(keywordFile)) return false;
+            if (!FileExists(jobdescFile)) return false;
+            if (!Directory.Exists(cvFolder)) return false;
+            return true;
+        }
+        private async void btnStartEvaluation_Click(object sender, EventArgs e)
+        {
+            logFile = FixLogFile(deflogFile);
+            if (!ValidateFiles())
+            {
+                MessageBox.Show("Kontrollera att alla filer och mappar är valda korrekt.");
+                return;
+            }
+            progressBar1.Visible = true;
+            this.UseWaitCursor = true;
+            Cursor.Current = Cursors.WaitCursor;
+            try
+            {
+                string output = await Task.Run(() => StartPythonAsExeAndGetOutput());
+                textBoxOutput.Text = output; // runs on UI thread
+            }
+            finally
+            {
+                this.UseWaitCursor = false;
+                Cursor.Current = Cursors.Default;
+                progressBar1.Visible = false;
+            }
+            textBoxOutput.Text += $"\r\n";
+            textBoxOutput.Text += $"\r\nDetaljer finns i filen: {logFile}";
+        }
+
+        // File: Form1.cs
+        private string StartPythonAsExeAndGetOutput()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = PythonexePath,
+                Arguments = $"\"{cvFolder}\" \"{jobdescFile}\" \"{keywordFile}\" \"{logFile}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(psi))
+            {
+                if (process == null) return "Failed to start process.";
+                string output = process.StandardOutput.ReadToEnd();
+                string errors = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+                if (!string.IsNullOrEmpty(errors))
+                    output += "\r\nErrors:\r\n" + errors;
+                return output;
+            }
+        }
+
+
+        private string RunPythonScript() //string pythonExePath, string scriptPath, params string[] args
+        {
+            string pythonExePath = @"C:\Program Files\Python314\python.exe"; // Ange sökväg till python.exe
+            string scriptPath = @"C:\Dev\Python\RangordnaCV\RangordnaCV.py"; // Ange sökväg till ditt Python-skript
+            // Bygg argumentsträngen
+            string arguments = $"\"{scriptPath}\"";
+            //foreach (var arg in args)
+            //{
+            //    arguments += $" \"{arg}\"";
+            //}
+            arguments = arguments + $"\"{cvFolder}\" \"{jobdescFile}\" \"{keywordFile}\"";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = pythonExePath,       // t.ex. @"C:\Path\To\python.exe"
+                Arguments = arguments,          // script + argument
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(psi))
+            {
+                string output = process.StandardOutput.ReadToEnd();
+                string errors = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(errors))
+                {
+                    output += "\nErrors:\n" + errors;
+                }
+
+                return output;
+            }
+        }
+
+
+        private void btnClearOutput_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Clear();
+        }
+    }
+}
